10237a7a65c37d6686c4dcb4d7057192236e9273
km_func <95062303+KM-func@users.noreply.github.com>
2022-12-07 11:07:32 +0800

    Semicolons added

--- src/pages/Login-SignUp/LoginForm.jsx
+++ src/pages/Login-SignUp/LoginForm.jsx
@@ -1,109 +1,23 @@
-// import { useState } from 'react';
-// import Button from 'react-bootstrap/Button'; 
-// import { Form , Field } from "react-final-form";
-
-// export default function LoginForm(props){ 
-//     // add states here
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("")
-//     // add form validator here
-//     const emailValidator = (_email) =>{
-        
-//         if(_email === ""){
-//             document.getElementById("email-error-msg").innerText = "Email cannot be empty"
-//         } else if(!_email.includes("@") || !_email.includes("@")){
-//             document.getElementById("email-error-msg").innerText = "please enter a valid email"
-//         }
-//          else{
-//             setEmail(_email);
-//             document.getElementById("email-error-msg").innerText = "";
-//         }
-//     }
-//     const passwordValidator = (_password) =>{
-//         if(_password === ""){
-//             document.getElementById("password-error-msg").innerText = "Password cannot be empty"
-//         } else if(_password.length < 8){
-//             document.getElementById("password-error-msg").innerText = "Invalid Password"
-//         }
-//          else{
-//             setPassword(_password)
-//             document.getElementById("password-error-msg").innerText = "";
-//         }
-//     }
-
-//     return(
-//         <div className='form-div'>
-//              <div className='form-header'>
-//                 <h2>Log In </h2>
-//                 <p>Don't have an account? <a onClick={props.changeToLogin} href="/">Sign Up</a></p>
-//             </div>
-//             <Form onSubmit={(formObj) => {
-//                 console.log( formObj )
-//             }}>
-//                 {({handleSubmit}) => (
-//                     <form onSubmit={props.handleSubmit}> 
-//                         {/* <label>Email</label> */}
-//                         <Field name="email"> 
-//                             {({ input }) => (
-//                                     <input 
-//                                         name='name'
-//                                         id="email"
-//                                         onChange={e =>{emailValidator(e.target.value)}} 
-//                                         type="email" placeholder="Enter email"
-//                                         {...input}
-//                                         required />  
-//                             )} 
-//                         </Field>
-//                         {/* <p id='email-error-msg' className='error-msg'></p> 
-//                         <label>Password</label> */}
-//                         <Field name="password">
-                            
-//                             {({ input }) => (
-//                                     <input
-//                                         name='password'
-//                                         id="password" 
-//                                         onChange={e =>{passwordValidator(e.target.value)}}
-//                                         type="password" placeholder="Enter Password"
-//                                         {...input}
-//                                         required/>
-//                             )}
-                            
-//                         </Field>
-//                         {/* <p id='password-error-msg' className='error-msg'></p> */}
-//                         {/* Add the errors later on */}
-//                         <button variant="primary" type="submit" className='mt-3'>
-//                             Log In
-//                         </button>
-//                     </form> 
-//                 )} 
-//             </RForm>
-            
-//         </div>
-//     )
-// }
-
 import { useState } from "react";
 import { Form , Field } from "react-final-form";
-import EyeOpen from "../../assets/images/eye_open.svg"
-import EyeClosed from "../../assets/images/eye_closed.svg"
+import EyeOpen from "../../assets/images/eye_open.svg";
+import EyeClosed from "../../assets/images/eye_closed.svg";
 
 export default function LoginForm(props){
     const [showPassword, setShowPassword] = useState(false);
 
-    const onSubmit = (e) =>{
-        // debugger
-        console.log(e)
+    const onSubmit = (e) =>{ 
+        console.log(e);
     }
     const validate = values => {
         const errors = {}
         if (!values.email) {
-          errors.email = 'Email is Required'
-            // 
+          errors.email = 'Email is Required';
         } else if(!values.email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)){ 
-            errors.email = 'Invalid Email'
+            errors.email = 'Invalid Email';
         } 
         if (!values.password) { 
-            errors.password = 'Password is Required'
+            errors.password = 'Password is Required';
         }  
         return errors
     }
@@ -112,16 +26,16 @@ export default function LoginForm(props){
         if(error){
             document.getElementById(id).style.borderColor = "#D74444";
         } else if(error === undefined){
-            document.getElementById(id).style.borderColor = "#8B8C93" 
+            document.getElementById(id).style.borderColor = "#8B8C93";
         }
     }
     
     const togglePassword = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         if(showPassword === false){
-            setShowPassword(true)
+            setShowPassword(true);
         } else{
-            setShowPassword(false)
+            setShowPassword(false);
         }
     }
     return(
