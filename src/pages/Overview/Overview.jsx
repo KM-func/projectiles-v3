c6e5acca75f095863a6927c551d17156d2f9219e
km_func <95062303+KM-func@users.noreply.github.com>
2022-12-14 21:40:41 +0800

    Update Overview.jsx

--- src/pages/Overview/Overview.jsx
+++ src/pages/Overview/Overview.jsx
@@ -17,6 +17,18 @@ export default function Overview(props){
             </div>
             <div className="overview-body">
                 <div className="header">
+                    <div>
+                        <h2>Title</h2>
+                        <h2>Target Completion</h2>
+                    </div>
+                    <div>
+                        <h2>Created at:</h2>
+                        <h2>Project Status</h2>
+                    </div>
+                    <div>
+                        <button type="button">X</button>
+                        <button type="button">edit</button>
+                    </div>
                 </div>
                 <div className="body">
                     <div className="description">
@@ -37,10 +49,29 @@ export default function Overview(props){
                     </div>
                     <div className="kanban">
                         <div className="header">
-                        
+                            Kanban Board 
+                            <button type="button">+</button>
                         </div>
-                        <div className="body">
-    
+                        <div className="board">
+                            <div className="list">
+                                <div className="list-item">
+                                    <h3>Delete this</h3>
+                                    <p>Assigned to: AK Mana</p>
+                                    <p className="grayed">Deadline: August 22, 2022</p>
+                                </div>
+                                <div className="list-item">
+                                    <h3>Delete this</h3>
+                                    <p>Assigned to: AK Mana</p>
+                                    <p className="grayed">Deadline: August 22, 2022</p>
+                                </div>
+                            </div>
+                            <div className="list">
+                                <div className="list-item">
+                                    <h3>Delete this</h3>
+                                    <p>Assigned to: AK Mana</p>
+                                    <p className="grayed">Deadline: August 22, 2022</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="details">
